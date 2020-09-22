badb0d1f1f8a78c9397d3bcdbd4be64af2e12dc0
Bruno-Felix <balvesfelix@gmail.com>
2020-09-22 17:25:31 -0300

    Modificando mensagem de início do BackEnd

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World teste Vamos Cuidar');
+    res.send('Vamos Cuidar Back End');
 });
     
 app.listen(PORT, HOST);
