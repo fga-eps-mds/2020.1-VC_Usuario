7cf98a0eccebd7c16faa6787b3b8af376181d5de
Bruno-Felix <balvesfelix@gmail.com>
2020-09-24 12:44:29 -0300

    Inserindo mongodb do docker-compose e criando arquivo Makefile

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const PORT = 8000;
 const HOST = '0.0.0.0';
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:false }));
+
 app.get('/', (req, res) => {
-    res.send('Vamos Cuidar Back End');
+    res.send('Funcionando!!');
 });
-    
+
+const mongoose = require('mongoose');
+
+// MongoDB connection
+mongoose
+    .connect(
+        'mongodb://mongo:27017/backend',
+        { useNewUrlParser: true }
+    )
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
 app.listen(PORT, HOST);
-console.log('Vamos Cuidar BackEnd inicializado com sucesso')
