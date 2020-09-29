0efe31e4a663eecd39d841ecb7e0164534ebf54d
DanielPortods <danielportods@gmail.com>
2020-09-29 00:08:36 -0300

    #104 Criando up-load de imagens.

--- src/controllers/postage.js
+++ src/controllers/postage.js
@@ -3,9 +3,10 @@ const Postage = require ('../models/postage.js');
 module.exports = {
     async an_post (req, res){
         try{
-            
+            console.log(req.file);
+            req.body.fk_id_usuario = null;
             const postage = await Postage.create(req.body);
-            //console.log(postage);
+            console.log(postage);
             return res.status(200).json({postage});
             
         }catch(err){
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
+const multer = require('multer');
 const Postage = require('./controllers/postage');
+const multerConfig = require('./config/multer');
 
-router.post('/an_post', Postage.an_post);
+router.post('/an_post', multer(multerConfig).single("file"), Postage.an_post);
 
 module.exports = router;
