1533e03d4671dd045abd4b0da51c6a814f06c0e4
enzoggqs <eggqsaraiva@gmail.com>
2020-10-10 18:01:24 -0300

    #126 Criptografando a senha no BackEnd
    Co-authored-by: Daniel Porto <danielportods@gmail.com>

--- src/models/user.js
+++ src/models/user.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.ObjectId;
+const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
-    unique_id:{
+    /* unique_id:{
         type: ObjectId,
         required: true,
-    },
+    }, */
     user_email:{
         type: String,
         required: true,
@@ -13,7 +14,8 @@ const UserSchema = new mongoose.Schema({
     user_password:{
         type: String,
         required: true,
-        select: false,
+        /* select: false, */
+
     },
     user_name:{
         type: String,
@@ -21,8 +23,16 @@ const UserSchema = new mongoose.Schema({
     },
     user_score:{
         type: Number,
-        required:true,
+        default: 0,
     }
+});
+
+UserSchema.pre('save', async function (next){
+    console.log('saddsah');
+    const hash = await bcrypt.hash(this.user_password, 10);
+    this.user_password = hash;
+
+    next();
 });
 
 const User = mongoose.model('User', UserSchema);
