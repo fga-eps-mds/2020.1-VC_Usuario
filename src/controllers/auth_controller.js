c176bcd5b37dc96b3357e7c90f859215ca5b1ff8
DanielPortods <danielportods@gmail.com>
2020-10-14 19:49:25 -0300

    #130 Corrigindo os códigos das respostas HTTP.

--- src/controllers/auth_controller.js
+++ src/controllers/auth_controller.js
@@ -5,17 +5,15 @@ const bcrypt = require('bcrypt');
 
 module.exports = {
     async authentication (req, res) {
-        //const {user_email, user_password} = req.body
 
         const user = await Users.findOne({ user_email: req.body.email })
-        console.log(user);
 
         if(!user) {
-            return res.status(500).send({ msg: 'Usuário não cadastrado.' })
+            return res.status(401).send({ msg: 'Usuário não cadastrado.' })
         }
 
         if(!await bcrypt.compare(req.body.password, user.user_password)){
-            return res.status(500).send({ msg: 'Senha inválida.' })
+            return res.status(401).send({msg: 'senha invalida'})
         }
 
         user.user_password = undefined
@@ -24,24 +22,26 @@ module.exports = {
             expiresIn: 7200,
         });
 
-        res.status(200).json({user, token})
+        console.log(token)
+
+        res.status(200).json({user, token, msg: 'Bem vindo!'})
     },
 
     async session_authentication (req, res, next) {
         const auth_token = req.headers.autorization;
 
         if(!auth_token)
-            res.status(401).send({error: 'Permissão negada'});
+            res.status(401).send({msg: 'Permissão negada'});
 
         const token_parts = auth_token.split(" ");
 
         if(token_parts.length != 2)
-            res.status(401).send({error: 'Algo de errado não está certo'});
+            res.status(401).send({msg: 'Algo de errado não está certo'});
 
         const [ scheme, token] = token_parts;
 
         if(!/^Bearer$/i.test(scheme))
-            res.status(401).send({error: 'Token mal formatado'});
+            res.status(401).send({msg: 'Token mal formatado'});
 
         JWT.verify(token, auth_config.secret, (err, decoded) => {
             if (err) return res.status(401).send({error: 'Token inválido'});
