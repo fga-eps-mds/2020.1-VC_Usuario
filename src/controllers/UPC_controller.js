eff76e31b311f4a615aee60763418d936a706ac4
Bruno-Felix <balvesfelix@gmail.com>
2020-11-16 23:40:29 -0300

    #192 Aprimorando método comment_postage e list_all | Criando método delete_all na controller - UPC_controller

--- src/controllers/UPC_controller.js
+++ src/controllers/UPC_controller.js
@@ -10,10 +10,35 @@ module.exports = {
 
     async list_all (req, res){
         const upc = await UPC.find();
-        return res.status(200).send("UPC list_all route successfully tested!");
+        return res.status(200).json(upc);
     },
 
-    async comment_postage(req, res){
-        return res.status(200).send("UPC comment_postage route successfully tested!")
+    async delete_all (req, res){
+        const upc = await UPC.deleteMany({})
+        return res.send(upc);
+    },
+
+    async comment_postage (req, res){
+
+        try{
+            console.log("Comment Postage...")
+
+            const postage_related_upc = await Postage.findById(req.body.postage_id)
+
+            const new_UPC = await UPC.create({
+                fk_user_id: req.body.user_id, 
+                fk_postage_id: req.body.postage_id,
+                UPC_description: req.body.comment_descripton
+            })
+
+            postage_related_upc.post_comments.unshift(new_UPC._id)
+            await postage_related_upc.updateOne({post_comments: postage_related_upc.post_comments});
+
+            console.log("New UPC successfully created!\n" + "\n-----\n")
+
+            return res.status(200).send("Comentário à Postagem " + postage_related_upc.post_title + " foi feito!");
+        }catch(err){
+            return res.status(400).send({error_comment_postage: err.message});
+        }
     }
 }
