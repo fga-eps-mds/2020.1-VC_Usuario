825f5d8df4daaab78faf5a5b59264a38f4c008d9
Daniel Barcelos <danielbarcelos.7@hotmail.com>
2020-12-01 08:53:23 -0300

    #162 Modificando testes de rota para postagem

--- __TEST__/routes/postages_routes.test.js
+++ __TEST__/routes/postages_routes.test.js
@@ -3,18 +3,21 @@ const request = require('supertest');
 const { response } = require('../../src/app');
 const app = require('../../src/app');
 const Postage = require('../../src/db/models/postage')
+const Ups = require('../../src/db/models/UPS')
+const User = require('../../src/db/models/user')
+
 const posts = [{
-        fk_user_id: '5faddfeaf110e8001879a324',
+        fk_user_id: null,
         post_place: 'FGA',
         post_category: 'Segurança',
-        post_title: 'Teste com jest',
+        post_title: 'Postagem Anônima',
         post_description: 'Mollit ipsum consectetur aliquip qui tempor excepteur. Elit enim do sit incididunt elit laborum fugiat labore adipisicing magna aute dolore. Velit ipsum consectetur labore ullamco ea eu deseruntl aborum ut.'
     },
     {
         fk_user_id: '5faddfeaf110e8001879a324',
         post_place: 'FGA',
         post_category: 'Limpeza',
-        post_title: 'Teste com jest',
+        post_title: 'Postagem comum',
         post_description: 'Mollit ipsum consectetur aliquip qui tempor excepteur.'
     },
     {
@@ -27,29 +30,34 @@ const posts = [{
     }
 ]
 
+let postAnom, postCommon;
+
 
 beforeEach(async () => {
 // Para testar a listagem, como devo definir um user_id como null?
     await Postage.deleteMany({})
     // posts[0].fk_user_id = null
-    await Postage(posts[0]).save();
-    await Postage(posts[1]).save();
-    await Postage(posts[2]).save();
+    // await Postage(posts[0]).save();
+    // await Postage(posts[1]).save();
+    // await Postage(posts[2]).save();
+    postAnom = await Postage.create(posts[0])
+    postCommon = await Postage.create(posts[1])
 });
 
 it('Anon post without image', async (done) => {
-    await request(app).post('/postage/create_anon').send({
+    const response = await request(app).post('/postage/create_anon').send({
         post_place: 'DARCY',
         post_category: 'Limpeza',
         post_title: 'Dreamcatcher lendas',
         post_description: 'Dream catcher perfeitas.'
     })
     .expect(200)
+    expect(response.body.fk_user_id).toBeNull()
     done()
 })
 
 it('Common post without image', async (done) => {
-    await request(app).post('/postage/create_common').send({
+    const response = await request(app).post('/postage/create_common').send({
         fk_user_id: '7eaddfeaf110e8001879a324',
         post_place: 'FCE',
         post_category: 'Segurança',
@@ -57,49 +65,61 @@ it('Common post without image', async (done) => {
         post_description: 'As maiores do kpop.'
     })
     .expect(200)
+    expect(response.body.fk_user_id).toEqual(expect.anything())
     done()
 })
 
 it('List all posts', async (done) => {
     const response = await request(app).get('/postage/list_all')
     .expect(200)
+    expect(response.body).toHaveLength(2)
     done()
 })
 
 it('List common posts', async (done) => {
     const response = await request(app).get('/postage/list_common')
     .expect(200)
+    expect(response.body.posts).toHaveLength(1)
     done()
 })
 
 it('List posts by category', async (done) => {
     // Como listar mais de uma postagem com base nas categorias?
     // O response.body não está retornando nenhuma postagem. Como passar a categoria que quero filtrar?
-    const response = await request(app).get('/postage/list_by_category')
+    await request(app).get('/postage/list_by_category')
     .expect(200)
+    // expect(response.body.posts.post_category).toBe('Segurança')
     done()
 })
 
 it('List one post by id', async (done) => {
-    // Preciso de um id de postagem para realizar a filtragem
-    const response = await request(app).post('/postage/create_anon').send(posts[0])
-
-    await request(app).get(`/postage/list_one/${response.body._id}`)
+    await request(app).get(`/postage/list_one/${postCommon._id}`)
     .expect(200)
     done()
 })
 
 it('List all posts with UPS', async (done) => {
-    // Como criar uma UPS para testar se essa rota funciona?
-    // Retorna 404 not found
+    const user = {
+        user_name: 'Sojin',
+        user_email: 'sojin@vc.com',
+        user_password: 'teste123'
+    }
+    const createdUser = await User.create(user)
 
-    await request(app).get(`/postage/list_all/${posts[1].fk_user_id}`)
-    .expect(404)
+    const ups = {
+        fk_user_id: createdUser._id,
+        fk_postage_id: postCommon._id
+    }
+    const createdUps = await Ups.create(ups)
+
+    const response = await request(app).get(`/postage/list_all_with_UPS/${createdUps.fk_user_id}`)
+    .expect(200)
+    expect(response.body).toHaveLength(1)
     done()
 })
 
 it('Delete all posts', async (done) => {
-    const response = await request(app).delete(`/postage/delete_all`)
+    const response = await request(app).delete('/postage/delete_all')
     .expect(200)
     done()
 })
@@ -109,7 +129,7 @@ it('Update post status', async (done) => {
     const response = await request(app).post('/postage/create_common').send(posts[1])
 
     await request(app).put(`/postage/update_status/${response.body._id}`)
-    .send(response.body.post_status = 'Resolvido')
+    .send(post_status = 'Resolvido')
     .expect(200)
     done()
 })
