57dadd81770a9daff91c62cafd149cc4b06c49bc
Daniel Barcelos <danielbarcelos.7@hotmail.com>
2020-11-30 21:11:00 -0300

    #162 Inserindo testes de rota para ups

---
+++ __TEST__/routes/ups_routes.test.js
@@ -0,0 +1,67 @@
+const request = require('supertest');
+const app = require('../../src/app');
+const Ups = require('../../src/db/models/UPS')
+
+const ups = {
+    fk_user_id: '5faddfeaf110e8001879a325',
+    fk_postage_id: '5fc5830cdea241e70a64071d'
+}
+
+beforeEach(async () => {
+    createdUps = await Ups.create(ups)
+});
+
+it('Create ups', async (done) => {
+    await request(app).post('/ups/create').send({
+        fk_user_id: '5faddfeaf110e8001879a324',
+        fk_postage_id: '5fc5830cdea241e70a64071c'
+    })
+    .expect(200)
+    done()
+})
+
+it('List all ups', async (done) => {
+    await request(app).get('/ups/list_all')
+    .expect(200)
+    done()
+})
+
+it('Delete all ups', async (done) => {
+    await request(app).delete('/ups/delete_all')
+    .expect(200)
+    done()
+})
+
+it('Support postage', async (done) => {
+    const user = await request(app).post('/user/register_user').send({
+        user_name: 'UPS Test',
+        user_email: 'ups@vc.com',
+        user_password: 'ups123'
+    })
+
+    const post = await request(app).post('/postage/create_common').send({
+        fk_user_id: user.body.User._id,
+        post_place: 'FCE',
+        post_category: 'Segurança',
+        post_title: 'T-ara',
+        post_description: 'As maiores do kpop.'
+    })
+
+    const res = await request(app).put('/ups/support_postage')
+    .send({
+        user_id: post.body.fk_user_id,
+        postage_id: post.body._id
+    })
+    expect(res.text).toBe('Apoio da Postagem T-ara foi modificado')
+    done()
+})
+
+afterEach(async(done) => {
+    await Ups.deleteMany({})
+    done()
+})
+
+afterAll((done) => {
+    console.log("All UPS tests is done.")
+    done();
+});
