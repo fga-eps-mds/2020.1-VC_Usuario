c0f19d3d72937a7d25367861433b222486fadc85
DanielPortods <danielportods@gmail.com>
2020-12-06 15:55:48 -0300

    #162 melhorando casos de testes do ups.

--- __TEST__/routes/ups_routes.test.js
+++ __TEST__/routes/ups_routes.test.js
@@ -20,6 +20,13 @@ it('Create ups', async (done) => {
     done()
 })
 
+it('Erro create ups', async(done) => {
+    await request(app).post('/ups/create').send({
+        fk_postage_id: '5fc5830cdea241e70a64071c'
+    }).expect(400)
+    done()
+})
+
 it('List all ups', async (done) => {
     await request(app).get('/ups/list_all')
     .expect(200)
@@ -37,7 +44,13 @@ it('Support postage', async (done) => {
         user_name: 'UPS Test',
         user_email: 'ups@vc.com',
         user_password: 'ups123'
-    })
+    }).expect(200)
+
+    const user2 = await request(app).post('/user/register_user').send({
+        user_name: 'UPS Test2',
+        user_email: 'ups2@vc.com',
+        user_password: 'ups123'
+    }).expect(200)
 
     const post = await request(app).post('/postage/create_common').send({
         fk_user_id: user.body.User._id,
@@ -45,13 +58,20 @@ it('Support postage', async (done) => {
         post_category: 'Segurança',
         post_title: 'T-ara',
         post_description: 'As maiores do kpop.'
-    })
+    }).expect(200)
 
     const res = await request(app).post('/ups/support_postage')
     .send({
-        fk_user_id: post.body.fk_user_id,
+        fk_user_id: user.body.User._id,
         fk_postage_id: post.body._id
     })
+    expect(res.text).toBe('Apoio da Postagem T-ara foi modificado')
+
+    const res2 = await request(app).post('/ups/support_postage')
+    .send({
+        fk_user_id: user.body.User._id,
+        fk_postage_id: post.body._id
+    }).expect(200)
     expect(res.text).toBe('Apoio da Postagem T-ara foi modificado')
     done()
 })
