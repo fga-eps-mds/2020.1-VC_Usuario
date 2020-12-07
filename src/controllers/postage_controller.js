86b0b50def256cf53981498946dcc83a3b6bae9b
DanielPortods <danielportods@gmail.com>
2020-12-07 19:57:06 -0300

    #196 Tornando o nome do autor da postagem dinamico.
    Co-authored-by: Daniel Barcelos <danielbarcelos.7@hotmail.com>

--- src/controllers/postage_controller.js
+++ src/controllers/postage_controller.js
@@ -241,8 +241,22 @@ module.exports = {
 
     async list_UPCs_by_postage (req, res){
         try{
-            const UPC_list = await UPC.find({ fk_postage_id: req.params.id })
-
+            const list = await UPC.find({ fk_postage_id: req.params.id })
+            
+            let i = 0
+            let UPC_list = []
+            for (upc in list){
+                const user = await User.findById(list[upc].fk_user_id)
+                
+                if(!user){
+                    continue
+                }
+                console.log(i)
+                list[upc].UPC_author = user.user_name  
+                UPC_list[i] = list[upc]    
+                i++
+            }
+            
             return res.status(200).json(UPC_list);
         }catch(err){
             return res.status(400).send({error_list_UPCs_by_postage: err.message}); 
