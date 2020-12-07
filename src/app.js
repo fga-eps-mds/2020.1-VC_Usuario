a915a9606cde795b3995cb9e81047a3144c79fc6
DanielPortods <danielportods@gmail.com>
2020-12-07 15:48:47 -0300

    #162 removendo configurações não utilizadas do cors.

--- src/app.js
+++ src/app.js
@@ -8,12 +8,6 @@ const path = require('path');
 const app = express();
 
 app.use(cors());
-/* app.use((req, res,) => {
-    console.log("midleware!");
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');  
-    app.use(cors()); 
-}); */ 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:false }));
 app.use('/img', express.static(path.resolve(__dirname, '..', 'tmp', 'img_uploads'))); //url de imagens
